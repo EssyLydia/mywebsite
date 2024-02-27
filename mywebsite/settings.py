4be9a2171c0cc3e19c855e1f91356ccaf817cc80
lydia <esterlydia4@gmaail.com>
2024-02-27 18:46:08 +0300

    second update on the website

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL  = '/images/'
+MEDIA_URL  = 'images/'
 
 STATIC_DIRS =[
     os.path.join(BASE_DIR, 'static')
